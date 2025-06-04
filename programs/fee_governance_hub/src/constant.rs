cfdebbe05c49e3f98186ef5b8696d10b3880d8b7
CryptoCooker <luongquy52.vn@gmail.com>
2025-06-04 15:40:05 +0700

    fix: remove feature:mainnet

--- programs/fee_governance_hub/src/constant.rs
+++ programs/fee_governance_hub/src/constant.rs
@@ -6,19 +6,19 @@ pub const MAX_FEE_INSTRUCTION_NAME_LEN: usize = 30;
 pub const PERCENT_DENOMINATOR: u64 = 1000;
 
 /// constants for admin wallets
-#[cfg(feature = "mainnet")]
+#[cfg(not(feature = "devnet"))]
 pub const ADMINS: [&str; 1] = [
   "F1tyGduCd9XLBSw2uAFycdNRUucyr2C7MiJ1Nifm2rZ8"
 ];
 
-#[cfg(feature = "mainnet")]
+#[cfg(not(feature = "devnet"))]
 pub const GLOBAL_FEE_WALLETS: [&str; 3] = [
   "ArpaDqpkJpKfxLP7WoFvYMbkj33C1PAHcy8tyrxFpgrc",
   "11111111111111111111111111111111",
   "11111111111111111111111111111111"
 ];
 
-#[cfg(feature = "mainnet")]
+#[cfg(not(feature = "devnet"))]
 pub const GLOBAL_FEE_WALLETS_FEE_PERCENT: [u64; 3] = [
   1000,
   0,
